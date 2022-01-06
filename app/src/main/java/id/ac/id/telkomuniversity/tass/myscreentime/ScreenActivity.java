93ad7e35c2d78d5d048267994d9d6abf32791272
Dani <erdaulhaqdani@gmail.com>
2022-01-06 22:33:42 +0700

    layout ScreenTime

---
+++ app/src/main/java/id/ac/id/telkomuniversity/tass/myscreentime/ScreenActivity.java
@@ -0,0 +1,26 @@
+package id.ac.id.telkomuniversity.tass.myscreentime;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScreenActivity extends AppCompatActivity {
+    private TextView mHasilST;
+    private Button mButtonTrack;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_st);
+
+        mHasilST=findViewById(R.id.textViewHasilST);
+        mButtonTrack=findViewById(R.id.button_start_track);
+
+
+
+    }
+}
